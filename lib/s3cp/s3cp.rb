3b8a4a87d21957e76f4b8412d833848d8d7178e7
Alex Boisvert <alex.boisvert@gmail.com>
2011-04-18 15:53:22 -0700

    Working s3cp

--- lib/s3cp/s3cp.rb
+++ lib/s3cp/s3cp.rb
@@ -3,6 +3,9 @@ require 'right_aws'
 require 'optparse'
 require 'date'
 require 'highline/import'
+require 'fileutils'
+
+require 's3cp/utils'
 
 # Parse arguments
 options = {}
@@ -11,6 +14,10 @@ options[:verbose] = true if $stdout.isatty
 op = OptionParser.new do |opts|
   opts.banner = "s3cp [path]"
   opts.separator ''
+
+  opts.on("-r", "Recursive mode") do
+    options[:recursive] = true
+  end
 
   opts.on("--verbose", "Verbose mode") do
     options[:verbose] = true
@@ -27,42 +34,134 @@ op = OptionParser.new do |opts|
 end
 op.parse!(ARGV)
 
-unless ARGV.size > 0
+if ARGV.size < 2
   puts op
   exit
 end
 
-url = ARGV[0]
+destination = ARGV.last
+sources = ARGV[0..-2]
+
+def s3?(url)
+  S3CP.bucket_and_key(url)[0]
+end
 
 if options[:debug]
   puts "URL: #{url}"
   puts "Options: \n#{options.inspect}"
 end
 
-fail "Your URL looks funny, doesn't it?" unless url =~ /s3:\/\/([^\/]*)\/?(.*)/
 @bucket = $1
 @prefix = $2
 
 @s3 = S3CP.connect()
 
-rows = 0
-@s3.interface.incrementally_list_bucket(@bucket, :prefix => @prefix) do |page|
-  page[:contents].each do |entry|
-    key = entry[:key]
-    last_modified = DateTime.parse(entry[:last_modified])
-    if options[:long_format]
-      puts "#{last_modified.strftime(options[:date_format])} #{key}"
+def direction(from, to)
+  if s3?(from) && s3?(to)
+    :s3_to_s3
+  elsif !s3?(from) && s3?(to)
+    :local_to_s3
+  elsif s3?(from) && !s3?(to)
+    :s3_to_local
+  else
+    :local_to_local
+  end
+end
+
+def no_slash(path)
+  path = path.match(/\/$/) ? path[0..-2] : path
+  path.match(/^\//) ? path[1..-1] : path
+end
+
+def relative(base, path)
+  no_slash(path[base.length..-1])
+end
+
+def copy(from, to, options)
+  bucket_from, key_from = S3CP.bucket_and_key(from)
+  bucket_to, key_to = S3CP.bucket_and_key(to)
+
+  #puts "bucket_from #{bucket_from.inspect} key_from #{key_from.inspect}"
+  #puts "bucket_to #{bucket_to.inspect} key_from #{key_to.inspect}"
+  #puts "direction #{direction(from, to)}"
+
+  case direction(from, to)
+  when :s3_to_s3
+    if options[:recursive]
+      keys = []
+      @s3.interface.incrementally_list_bucket(bucket_from, :prefix => key_from) do |page|
+        page[:contents].each { |entry| keys << entry[:key] }
+      end
+      keys.each do |key|
+        dest = no_slash(key_to) + '/' + relative(key_from, key)
+        puts "Copy s3://#{bucket_from}/#{key} to s3://#{bucket_to}/#{dest}"
+        @s3.interface.copy(bucket_from, key, bucket_to, dest)
+      end
     else
-      puts key
+      puts "Copy s3://#{bucket_from}/#{key_from} to s3://#{bucket_to}/#{key_to}"
+      @s3.interface.copy(bucket_from, key_from, bucket_to, key_to)
     end
-    rows += 1
-    if options[:rows_per_page] && (rows % options[:rows_per_page] == 0)
+  when :local_to_s3
+    if options[:recursive]
+      files = Dir[from + "/**/*"]
+      files.each do |f|
+        f = File.expand_path(f)
+        key = no_slash(key_to) + '/' + relative(from, f)
+        puts "Copy #{f} to s3://#{bucket_to}/#{key}"
+        @s3.interface.put(bucket_to, key,  File.open(f))
+      end
+    else
+      f = File.expand_path(from)
+      puts "Copy #{f} to s3://#{bucket_to}/#{key_to}"
+      f = File.open(f)
       begin
-        print "Continue? (Y/n) "
-        response = STDIN.gets.chomp.downcase
-      end until response == 'n' || response == 'y' || response == ''
-      break if response == 'n'
+        @s3.interface.put(bucket_to, key_to, f)
+      ensure
+        f.close()
+      end
+    end
+  when :s3_to_local
+    if options[:recursive]
+      keys = []
+      @s3.interface.incrementally_list_bucket(bucket_from, :prefix => key_from) do |page|
+        page[:contents].each { |entry| keys << entry[:key] }
+      end
+      keys.each do |key|
+        dest = File.expand_path(to) + '/' + relative(key_from, key)
+        dest = File.join(dest, File.basename(key)) if File.directory?(dest)
+        puts "Copy s3://#{bucket_from}/#{key} to #{dest}"
+        f = File.new(dest, File::CREAT|File::RDWR)
+        begin
+          @s3.interface.get(bucket_from, key) do |chunk|
+            f.write(chunk)
+          end
+        ensure
+          f.close()
+        end
+      end
+    else
+      dest = File.expand_path(to)
+      dest = File.join(dest, File.basename(key_from)) if File.directory?(dest)
+      puts "Copy s3://#{bucket_from}/#{key_from} to #{dest}"
+      f = File.new(dest, File::CREAT|File::RDWR)
+      begin
+        @s3.interface.get(bucket_from, key_from) do |chunk|
+          f.write(chunk)
+        end
+      ensure
+        f.close()
+      end
+    end
+  when :local_to_local
+    if options[:recursive]
+      FileUtils.cp_r from, to
+    else
+      FileUtils.cp from, to
     end
   end
+end
+
+sources.each do |source|
+  copy(source, destination, options)
 end
 
