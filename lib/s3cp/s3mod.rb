2090db17e6d3db37a2660d58db4d15bd46b7d2d5
Alex Boisvert <alex.boisvert@gmail.com>
2011-10-17 17:29:05 -0700

    Improve s3mod help

--- lib/s3cp/s3mod.rb
+++ lib/s3cp/s3mod.rb
@@ -1,10 +1,20 @@
-require 'rubygems'  
-require 'right_aws'  
+require 'rubygems'
+require 'extensions/kernel' if RUBY_VERSION =~ /1.8/
+require 'right_aws'
 require 'optparse'
 require 's3cp/utils'
 
 op = OptionParser.new do |opts|
   opts.banner = "s3mod [path] [permission]"
+
+  opts.separator ""
+  opts.separator "where [permission] is one of:"
+  opts.separator ""
+  opts.separator " * private"
+  opts.separator " * authenticated-read"
+  opts.separator " * public-read"
+  opts.separator " * public-read-write"
+  opts.separator ""
 
   opts.on_tail("-h", "--help", "Show this message") do
     puts op
@@ -19,10 +29,10 @@ if ARGV.size < 2
   exit
 end
 
-def update_permissions(s3, bucket, key, permission)  
+def update_permissions(s3, bucket, key, permission)
   puts "Setting #{permission} on s3://#{bucket}/#{key}"
-  s3.interface.copy(bucket, key, bucket, key, :replace, {"x-amz-acl" => permission })  
-end  
+  s3.interface.copy(bucket, key, bucket, key, :replace, {"x-amz-acl" => permission })
+end
 
 source  = ARGV[0]
 permission = ARGV.last
@@ -32,4 +42,4 @@ raise "Permissions must be one of the following values: #{LEGAL_MODS}" unless LE
 
 @s3 = S3CP.connect()
 bucket,key = S3CP.bucket_and_key(source)
-update_permissions(@s3, bucket, key, permission)  
+update_permissions(@s3, bucket, key, permission)
